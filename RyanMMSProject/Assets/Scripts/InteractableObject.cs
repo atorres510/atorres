f4365cf413d0ce7a7cef46760a67d62e7f7d11c4
atorres510 <torre157@umn.edu>
2015-06-04 01:21:30 -0500

    Added RyanMMSProject

---
+++ RyanMMSProject/Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InteractableObject : MonoBehaviour {
+
+
+	Light simpleHighlightEffect;
+	public Text hoverOverText;
+
+	public string test;
+
+
+
+
+
+	void OnMouseOver(){
+	
+	
+		simpleHighlightEffect.enabled = true;
+	
+	
+	
+	
+	}
+
+
+
+
+	void OnMouseExit(){
+	
+		simpleHighlightEffect.enabled = false;
+	
+	
+	}
+
+
+
+
+
+
+	void SetupObject(){
+	
+
+
+	
+	
+	}
+
+
+
+
+
+
+	void Start () {
+
+		simpleHighlightEffect = gameObject.GetComponent<Light> ();
+		simpleHighlightEffect.enabled = false;
+
+		int stringLength = test.Length;
+
+		Debug.Log (stringLength);
+
+	
+	}
+	
+
+
+
+
+	void Update () {
+	
+	}
+}
---
+++ RyanMMSProject/Assets/Scripts/Textbox.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Textbox : MonoBehaviour {
+
+	public Text UIText;
+	public GameObject UIPanel;
+
+	string text;
+	string[] lines;
+
+	RectTransform rectTransform;
+	float unitPerLine = 15;
+	float unitPerChar = 6;
+
+
+
+
+
+
+	string[] CutIntoLines(string text){
+
+		string[] cutLines = text.Split (new char[] {'\n'}, System.StringSplitOptions.RemoveEmptyEntries);
+
+
+		for (int i = 0; i < cutLines.Length; i++) {
+		
+			//Debug.Log(cutLines[i]);
+		
+		
+		
+		
+		}
+
+		return cutLines;
+
+	}
+
+
+	int CountLines(string[] lines){
+	
+	
+		return (lines.Length + 1);
+	
+	
+	}
+
+
+
+	int CountCharacters(string line){
+	
+	
+		return line.Length;
+	
+	
+	
+	}
+
+
+	int ReturnLongestLineLength(string[] lines){
+	
+		string longestLineSoFar = "";
+
+
+		for (int i = 0; i < lines.Length; i++) {
+		
+			int characters = CountCharacters(lines[i]);
+
+			if(longestLineSoFar.Length < characters){
+
+				longestLineSoFar = lines[i];
+
+			}
+
+			else{/*pass*/}
+
+
+		}
+	
+		Debug.Log ("Longest Line So Far is: " + longestLineSoFar);
+	
+		return CountCharacters(longestLineSoFar);
+	
+	}
+
+
+
+	void FormatTextBoxSize (RectTransform r, int numberofChar, int numberOfLines){
+
+		//finds the right width by using the number of characters in the longest line
+		float rectWidth = numberofChar * unitPerChar;
+		//fids the right heigth of by finding number of lines
+		float rectHeigth = numberOfLines * unitPerLine;
+
+		rectTransform.sizeDelta = (new Vector2(rectWidth, rectHeigth));
+
+
+
+
+
+	}
+
+
+
+	void Start(){
+
+
+		rectTransform = gameObject.GetComponent<RectTransform> ();
+		lines = CutIntoLines (text);
+		FormatTextBoxSize(rectTransform, ReturnLongestLineLength(lines), CountLines(lines));
+
+
+		 
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
