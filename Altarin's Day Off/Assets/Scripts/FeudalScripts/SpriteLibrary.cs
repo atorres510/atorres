1420395a4f93e84418c0927417820b9028074f69
atorres510 <veryconfused@charter.net>
2015-12-01 00:15:28 -0600

    added dictionary to Sprite Library

--- Altarin's Day Off/Assets/Scripts/FeudalScripts/SpriteLibrary.cs
+++ Altarin's Day Off/Assets/Scripts/FeudalScripts/SpriteLibrary.cs
@@ -1,41 +1,85 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 
 public class SpriteLibrary : MonoBehaviour {
 
+	public string folderPath;
+
 	//Stores sprites in arrays by faction.  Uses piece Designation to ID piece sprite type.
-	
+	Dictionary<string, Sprite[]> factionSprites = new Dictionary<string, Sprite[]>();
 
 
-	void InstantiateSpriteArrays(){
 
-		string spritePath = "Assets/Resources/Sprites/FeudalSprites/FactionSprites"; 
+	void CreateSpriteDictionary(string path){
 
-		string[] filePaths = Directory.GetFiles (spritePath);
-
-		string[] factions; 
+		string[] filePaths = Directory.GetFiles (path);
 
 		foreach (string filePath in filePaths) {
-				
-			Debug.Log(filePath);
 
+			string cleanedPath = CleanString(filePath);
 
-		
+			Sprite[] sprites = ReturnSpriteArray(cleanedPath);
+
+			string key = cleanedPath.Substring(37);
+			key = key.Trim();
+
+			Debug.Log(key);
+
+			factionSprites.Add(key, sprites);
 		
 		}
 	
 
 	}
 
+	//cleans filepath, removing "Assets/Resources/", "\", and ".meta" from the string. also trims whitespace
+	string CleanString(string s){
+
+		//removes "Assets/Resources/"
+		string clean  = s.Substring(17);
+		//removes ".meta"
+		clean = clean.Remove(clean.Length - 5);
+		//removes "\"
+		string [] cleanedStrings = clean.Split ('\\');
+		//addes all the cleaned strings and connects the path
+		clean = cleanedStrings[0] + "/" + cleanedStrings[1];
+		//trims whitespace on ends
+		clean = clean.Trim ();
+
+		return clean;
+	
+	}
+
+
+
+
+	//reads path and creates a sprite array of the assets found
+	Sprite[] ReturnSpriteArray(string path){
+
+		Sprite[] sprites = Resources.LoadAll<Sprite>(path);
+
+		return sprites;
+
+	}
 
 
 	void Start(){
 
+		//string abc = "abcdefghijklm\nopqrstuvwxyz";
+		//string[] abcCut = abc.Split('\\');
 
-		//InstantiateSpriteArrays ();
+		//foreach (string word in abcCut) {
+				
+		//	Debug.Log(word);
+		
+		//}
 	
+		CreateSpriteDictionary (folderPath);
+	
+
 	}
 
 
