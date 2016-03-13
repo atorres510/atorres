ee2f740545d99ddd0f5462d83a076464e7f3f3ed
atorres510 <veryconfused@charter.net>
2016-03-13 00:04:22 -0600

    added new project, Bel-Nix Character Creator

---
+++ Bel-Nix Character Creator/Assets/CharacterCreator.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterCreator : MonoBehaviour {
+
+
+    public Renderer activeRenderer;
+
+
+    public Renderer skinRenderer;
+    public Renderer hairRenderer;
+    public Renderer vestRenderer;
+    public Renderer shirtRenderer;
+    public Renderer longShirtRenderer;
+    public Renderer accessoryRenderer;
+
+    public ColorPicker picker;
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        picker.onValueChanged.AddListener(color =>
+        {
+            //renderer.material.color = color;
+        });
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
