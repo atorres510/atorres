f25dbc5fca9b034830baefb4da9db892cbb51f8c
Andrew <torre157@umn.edu>
2017-12-16 18:01:05 -0600

    Added vault point sprites and modified vertical vaultpoint prefab.

--- TariensDayOff/Assets/Scripts/DoorBehavior.cs
+++ TariensDayOff/Assets/Scripts/DoorBehavior.cs
@@ -3,70 +3,75 @@ using System.Collections;
 
 public class DoorBehavior : MonoBehaviour {
 
+    [SerializeField]
+    private bool isDoorOpen; //determines open/closed stated. Can be set in the inspector if the door should start in a certain state.
+    
     //Components
-	public BoxCollider2D doorBoxCollider2D;
-    public SpriteRenderer mainSpriteRenderer;
-    public SpriteRenderer topSpriteRenderer;
+    public BoxCollider2D doorBoxCollider2D;
+    public SpriteRenderer mainSpriteRenderer; //This renderer comes from the sprite renderer on the door's collider.  Should contain the entire sprite
+    public SpriteRenderer topSpriteRenderer; //This renderer comes from the parent gameobject.  Should only contain the top of the sprite.
+
     //Sprite states
-    public Sprite openStateSprite;
+    public Sprite openStateSprite; //these two sprites are the toggled sprite states for the main sprite renderer
     public Sprite closedStateSprite;
-    public Sprite topOpenStateSprite;
+    public Sprite topOpenStateSprite;//these two sprites are the toggled sprite states for the top sprite renderer
     public Sprite topClosedStateSprite;
 
-
     private GameObject player;
-	//private BoxCollider2D boxcollider2D;
-
-    [SerializeField]
-	private bool isDoorOpen;
     
+   
 
     //Initialize variables
 	void Awake() {
 
-		player = GameObject.FindGameObjectWithTag ("Player");
-		doorBoxCollider2D.enabled = !isDoorOpen;
+		player = GameObject.FindGameObjectWithTag ("Player"); //find player.
+        CheckDoorState(); //checks if the door should start open or closed based on inspector variable setting.
 		
-
-
 	}
 
-
+    //checks if player is colliding with trigger collider, then toggles the 
+    //door open boolean if button is pressed.  Then calls CheckDoorState to "open" or "close" the door.  
 	void OnTriggerStay2D(Collider2D other){
-        //Debug.Log(other.name);
+        
         if (other.gameObject == player){
 
-            //Debug.Log("Player is colliding.");
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                Debug.Log("GetKeyDown");
-                if (!isDoorOpen) //Open Door State
-                {
-
-                    isDoorOpen = true;
-                    doorBoxCollider2D.enabled = false;
-                    mainSpriteRenderer.sprite = openStateSprite;
-                    topSpriteRenderer.sprite = topOpenStateSprite;
-                    Debug.Log("Door is open");
-
-                }
-                else if (isDoorOpen) //Closed Door State
-                {
-
-                    isDoorOpen = false;
-                    doorBoxCollider2D.enabled = true;
-                    mainSpriteRenderer.sprite = closedStateSprite;
-                    topSpriteRenderer.sprite = topClosedStateSprite;
-                    Debug.Log("Door is closed");
-
-                }
+                
+                isDoorOpen = !isDoorOpen;
+                CheckDoorState();
 
             }
             
         }
         
 	}
+
+    //Called when the door properties need to be changed.  Uses isDoorOpen.
+    void CheckDoorState() {
+
+        if (isDoorOpen) //Open Door State
+        {
+            
+            doorBoxCollider2D.enabled = false;
+            mainSpriteRenderer.sprite = openStateSprite;
+            topSpriteRenderer.sprite = topOpenStateSprite;
+            Debug.Log("Door is open");
+
+        }
+        else if (!isDoorOpen) //Closed Door State
+        {
+            
+            doorBoxCollider2D.enabled = true;
+            mainSpriteRenderer.sprite = closedStateSprite;
+            topSpriteRenderer.sprite = topClosedStateSprite;
+            Debug.Log("Door is closed");
+
+        }
+
+    }
     
+
 }
 
 
